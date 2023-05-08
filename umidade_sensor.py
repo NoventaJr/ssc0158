f8225f115e7ce3aa91982fee9ed21fccd38295b1
Rodrigo Noventa Júnior <noventajr@gmail.com>
2023-05-08 15:21:39 -0300

    Update and rename humidade.py to umidade_sensor.py

--- umidade_sensor.py
+++ umidade_sensor.py
@@ -4,13 +4,13 @@ from random import uniform
 import time
 
 #Configurando e conectando
-mqtt_broker = 'mqtt.eclipseprojects.io'
+mqtt_broker = 'test.mosquitto.org'
 mqqt_client = mqtt.client('MQTTProducer')
 mqtt_client.connect(mqtt_broker)
 
 #Simulando publicações de Humidade
 while true:
     randNumber = uniform(50.0, 80.0)
-    mqtt_client.publish("Humidade", randNumber)
-    print('MQTT publicou' + str(randNumber) + ' em Humidade.')
+    mqtt_client.publish("Umidade", randNumber)
+    print('MQTT publicou' + str(randNumber) + ' em Umidade.')
     time.sleep(3)
