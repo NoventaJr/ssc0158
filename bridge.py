f6115c8976fd81a6ce78a2f0fd0bc16db2e8b8ac
leonardovin <mcleozin4u@gmail.com>
2023-06-24 17:31:14 -0300

    multithread

--- bridge.py
+++ bridge.py
@@ -1,60 +1,65 @@
 import paho.mqtt.client as mqtt
 from pykafka import KafkaClient
 import time
+from threading import Thread
 
-# Configuring and connecting MQTT
+
+class MQTTBridge:
+    def __init__(self, mqtt_broker, kafka_broker):
+        self.mqtt_broker = mqtt_broker
+        self.kafka_client = KafkaClient(hosts=kafka_broker)
+
+    def bridge_mqtt_to_kafka(self, mqtt_topic, kafka_topic):
+        mqtt_client = mqtt.Client()
+        mqtt_client.connect(self.mqtt_broker)
+
+        kafka_topic = self.kafka_client.topics[kafka_topic]
+        kafka_producer = kafka_topic.get_sync_producer()
+
+        def on_message(client, userdata, message):
+            mqtt_topic = message.topic
+            payload = message.payload.decode("utf-8")
+
+            print("Received MQTT message from topic:", mqtt_topic)
+            print("Publishing to Kafka topic:", kafka_topic)
+
+            kafka_producer.produce(payload.encode("ascii"))
+
+        mqtt_client.on_message = on_message
+        mqtt_client.subscribe(mqtt_topic)
+        mqtt_client.loop_start()
+
+    def start_bridge(self):
+        mqtt_to_kafka_threads = [
+            Thread(
+                target=self.bridge_mqtt_to_kafka,
+                args=(mqtt_topic, kafka_topic),
+            )
+            for mqtt_topic, kafka_topic in self.mqtt_to_kafka_mappings
+        ]
+
+        for thread in mqtt_to_kafka_threads:
+            thread.start()
+
+        time.sleep(500)
+
+        for thread in mqtt_to_kafka_threads:
+            thread.join()
+
+
+# Configuring MQTT broker, Kafka broker, and MQTT to Kafka topic mappings
 mqtt_broker = "test.mosquitto.org"
-mqtt_client = mqtt.Client("MQTTBridge")
-mqtt_client.connect(mqtt_broker)
+kafka_broker = "localhost:9092"
+mqtt_to_kafka_mappings = [
+    ("Temperatura", "Temperatura"),
+    ("Vento", "Vento"),
+    ("Umidade", "Umidade"),
+    ("Chuva", "Chuva"),
+]
 
-# Configuring and connecting Kafka
-kafka_client = KafkaClient(hosts="localhost:9092")
+# Creating MQTTBridge instance
+mqtt_bridge = MQTTBridge(mqtt_broker, kafka_broker)
+mqtt_bridge.mqtt_to_kafka_mappings = mqtt_to_kafka_mappings
 
-# Topics
-kafka_temp_topic = kafka_client.topics[b"Temperatura"]
-kafka_wind_topic = kafka_client.topics[b"Vento"]
-kafka_hum_topic = kafka_client.topics[b"Umidade"]
-kafka_rain_topic = kafka_client.topics[b"Chuva"]
-
-# Producers
-kafka_temp_producer = kafka_temp_topic.get_sync_producer()
-kafka_wind_producer = kafka_wind_topic.get_sync_producer()
-kafka_hum_producer = kafka_hum_topic.get_sync_producer()
-kafka_rain_producer = kafka_rain_topic.get_sync_producer()
-
-
-# Function to handle MQTT messages
-def on_message(client, userdata, message):
-    topic = message.topic
-    payload = message.payload.decode("utf-8")
-
-    print("Received message from MQTT - Topic:", topic)
-    print("Received message from MQTT - Payload:", payload)
-
-    if "Temperatura" in topic:
-        kafka_temp_producer.produce(payload.encode("ascii"))
-        print("Message published to Temperatura topic in Kafka.")
-    elif "Vento" in topic:
-        kafka_wind_producer.produce(payload.encode("ascii"))
-        print("Message published to Vento topic in Kafka.")
-    elif "Umidade" in topic:
-        kafka_hum_producer.produce(payload.encode("ascii"))
-        print("Message published to Umidade topic in Kafka.")
-    elif "Chuva" in topic:
-        kafka_rain_producer.produce(payload.encode("ascii"))
-        print("Message published to Chuva topic in Kafka.")
-
-
-# Subscribing to MQTT topics
-mqtt_client.on_message = on_message
-
-mqtt_client.subscribe(
-    "+/+", qos=0
-)  # Single-level wildcard: "+/+" matches any single-level topic
-
-# Consuming MQTT messages
-mqtt_client.loop_start()
-
-time.sleep(500)
-
-mqtt_client.loop_stop()
+# Starting the MQTT to Kafka bridge
+mqtt_bridge.start_bridge()
