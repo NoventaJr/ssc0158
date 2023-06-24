69edb3e61dde46a4b1999b5dc490b7fd693e839a
leonardovin <mcleozin4u@gmail.com>
2023-06-24 18:48:08 -0300

    update: install script

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo apt install default-jdk
 
 wget https://downloads.apache.org/kafka/3.4.0/kafka_2.13-3.4.0.tgz
 tar xzf kafka_2.13-3.4.0.tgz
+rm xzf kafka_2.13-3.4.0.tgz
 sudo mv kafka_2.13-3.4.0 /usr/local/kafka 
 
 pip3 install paho-mqtt
