a88a72c66eac0b734f369d8b342ce37b0b532b35
leonardovin <mcleozin4u@gmail.com>
2023-06-24 19:50:34 -0300

    fix

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -3,13 +3,11 @@ from pykafka import KafkaClient
 # Connect to Kafka
 client = KafkaClient(hosts="localhost:9092")
 
-# Choose the topic to consume from
-topic_name = "Umidade"
+# Define the list of topics to consume from
+topics = ["Chuva", "Umidade", "Vento", "Temperatura"]
 
 # Create a consumer
-consumer = client.topics[
-    "Chuva".encode(), "Umidade".encode(), "Vento".encode(), "Temperatura".encode()
-].get_simple_consumer()
+consumer = client.topics[[topic.encode() for topic in topics]].get_simple_consumer()
 
 # Define the output file path
 output_file = "medidas.txt"
@@ -17,7 +15,7 @@ output_file = "medidas.txt"
 # Consume messages and write them to the output file
 for message in consumer:
     if message is not None:
-        file = open(message.topic + ".txt", "a")
+        file = open(message.topic.decode() + ".txt", "a")
         # Decode the message value assuming it's UTF-8 encoded
         message_value = message.value.decode("utf-8")
         print(message_value, "\n")
